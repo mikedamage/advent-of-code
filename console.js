476748a6862bd94c158901c684739521739b30a4
Mike Green <mike.is.green@gmail.com>
2015-12-10 09:51:20 -0500

    Add console bootstrap script

---
+++ console.js
@@ -0,0 +1,7 @@
+require('babel-register');
+
+var fs   = require('fs');
+var path = require('path');
+var day1 = require('./day-1/solve');
+var day2 = require('./day-2/solve');
+var day3 = require('./day-3/solve');
